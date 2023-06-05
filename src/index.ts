84408ca491112bde0380939e04704370090cfaf7
Juan Ignacio Moyano <nacho_moyano89@hotmail.com>
2023-06-05 10:49:00 -0300

    se agrego patineta en contenedor

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,62 @@
-import { Application, Loader, Sprite } from 'pixi.js'
+import { Application, Container, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1280,
+	height: 720
 });
 
+window.addEventListener("resize", ()=>{//Evento para centrar la imagen cuando se cambia el tamaño de la pantalla
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX, scaleY);//escala para calcular la imagen con el tamaño de pantalla que se tiene
+
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
+
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2);//modifica los margenes (los divide por dos para poner la mitad de cada margen en cada lado)
+
+	app.view.style.width = gameWidth + "px";
+	app.view.style.height = gameHeight + "px";//cambia la pantalla con la escala tomada
+
+	app.view.style.marginLeft = marginHorizontal + "px";
+	app.view.style.marginRight = marginHorizontal + "px";//cambia la pantalla con los margenes tomados
+
+	app.view.style.marginTop = marginVertical + "px";
+	app.view.style.marginBottom = marginVertical + "px";
+});
+window.dispatchEvent(new Event("resize"));
+
+
 Loader.shared.add({url: "./dino.png", name:"myDino"});
+Loader.shared.add({url: "./patineta.png", name:"patineta"});
 
 
 
 Loader.shared.onComplete.add(()=>{
-const clampy: Sprite = Sprite.from("myDino");
+const dino: Sprite = Sprite.from("myDino");
 
-clampy.anchor.set(0.5);
 
-clampy.x = 300;
-clampy.y = 200;
 
-app.stage.addChild(clampy);
+const patineta: Sprite = Sprite.from("patineta");
+
+patineta.position.set(10,100);
+patineta.scale.set(0.7,0.7);
+
+const dinoWithPatineta: Container = new Container();
+
+dinoWithPatineta.addChild(patineta);
+dinoWithPatineta.addChild(dino);
+
+dinoWithPatineta.x= 200;
+dinoWithPatineta.y= 300;
+
+app.stage.addChild(dinoWithPatineta);
+
 });
 
 Loader.shared.load();
